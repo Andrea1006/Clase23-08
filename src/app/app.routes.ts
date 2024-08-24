d40f02be988738d9ce27f1aa5bad297267907a2d
Andrea1006 <andrea_narvaeztr@fet.edu.co>
2024-08-23 22:05:11 -0500

    Correcciones clase23-08

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PagComponent } from './pages/pag/pag.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: PagComponent }
+];
--- src/app/pages/pag/pag.component.ts
+++ src/app/pages/pag/pag.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
+
 
 
 @Component({
   selector: 'app-pag',
   standalone: true,
-  imports: [ReactiveFormsModule,FormGroup, FormBuilder],
+  imports: [ReactiveFormsModule, NgFor],
   templateUrl: './pag.component.html',
-  styleUrl: './pag.component.css'
+  styleUrls: ['./pag.component.css']
 })
+
 export class PagComponent {
   formulario: FormGroup;
   datosIngresados: Array<{nombre: string, email: string}> = [];
